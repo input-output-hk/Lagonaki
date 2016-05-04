5dd0e59069970d45b412ec504a933e125b94a81a
catena <catena@protonmail.com>
2016-05-04 17:54:30 +0300

    /scorex/stop API route

--- src/test/scala/scorex/lagonaki/ScorexAPISpecification.scala
+++ src/test/scala/scorex/lagonaki/ScorexAPISpecification.scala
@@ -7,14 +7,18 @@ class ScorexAPISpecification extends FunSuite with Matchers {
 
   import TestingCommons._
 
+  test("/scorex/stop API route") {
+    POST.incorrectApiKeyTest("/scorex/stop")
+  }
+
   test("/scorex/status API route") {
-    val status = getRequest("/scorex/status")
+    val status = GET.request("/scorex/status")
     List("generating", "syncing") should contain((status \ "block_generator_status").as[String])
     List("synced", "syncing") should contain((status \ "history_synchronization_status").as[String])
   }
 
   test("/scorex/version API route") {
-    val version = getRequest("/scorex/version")
+    val version = GET.request("/scorex/version")
     (version \ "version").as[String].contains("Scorex") shouldBe true
     (version \ "version").as[String].contains("Lagonaki") shouldBe true
     (version \ "version").as[String].contains("v. 1.2.") shouldBe true
